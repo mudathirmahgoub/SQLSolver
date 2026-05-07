f2ccdf1c6a90d9cd6054365fe0498870cba809e4
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-05-06 23:31:57 -0500

    more

--- api/src/test/java/sqlsolver/api/lia/SqlSolverTest.java
+++ api/src/test/java/sqlsolver/api/lia/SqlSolverTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import sqlsolver.api.entry.Verification;
 import sqlsolver.sql.plan.Value;
 import sqlsolver.superopt.liastar.LiaSolver;
+import sqlsolver.superopt.liastar.LiaSolverStatus;
 import sqlsolver.superopt.liastar.LiaStar;
 import sqlsolver.superopt.logic.VerificationResult;
 
@@ -30,38 +31,62 @@ public class SqlSolverTest
 
   private final LiaStar x = LiaStar.mkVar(false, "x", Value.TYPE_INT);
   private final LiaStar y = LiaStar.mkVar(false, "y", Value.TYPE_INT);
-  private final LiaStar z = LiaStar.mkVar(false, "z", Value.TYPE_INT);
+  private final LiaStar a = LiaStar.mkVar(false, "a", Value.TYPE_INT);
+  private final LiaStar b = LiaStar.mkVar(false, "b", Value.TYPE_INT);
   private final LiaStar c17 = LiaStar.mkConst(false, 17);
   private final LiaStar c0 = LiaStar.mkConst(false, 0);
   private final LiaStar c1 = LiaStar.mkConst(false, 1);
-  private final LiaStar c5 = LiaStar.mkConst(false, 5);
   private final LiaStar c2 = LiaStar.mkConst(false, 2);
   private final LiaStar c3 = LiaStar.mkConst(false, 3);
+  private final LiaStar c4 = LiaStar.mkConst(false, 4);
+  private final LiaStar c5 = LiaStar.mkConst(false, 5); 
   private final LiaStar c6 = LiaStar.mkConst(false, 6);
   private final LiaStar c8 = LiaStar.mkConst(false, 8);
   private final LiaStar c20 = LiaStar.mkConst(false, 20);
   private final LiaStar c47 = LiaStar.mkConst(false, 47);
   private final LiaStar cMinus1 = LiaStar.mkConst(false, -1);
   private final LiaStar cMinus3 = LiaStar.mkConst(false, -3);
+  private final LiaStar cMinus5 = LiaStar.mkConst(false, -5);
 
   @Test
-  public void f2()
+  public void example1()
   {
-    // y >= x - 3 and y >= 3 - x
-    // 5x + 2 y >= 17
+    // y >= x - 3 and y >= 3 - x    
     var ineq1 = LiaStar.mkLe(false, LiaStar.mkPlus(false, x, cMinus3), y);
     var minusX = LiaStar.mkMul(false, cMinus1, x);
     var ineq2 = LiaStar.mkLe(false, LiaStar.mkPlus(false, c3, minusX), y);
     var body = LiaStar.mkConjunction(false, Arrays.asList(new LiaStar[] {ineq1, ineq2}));
 
-    // z = 1
-    var zEquals1 = LiaStar.mkEq(false, z, c1);
+    // (a,b) = (4,1)
+    var aEquals = LiaStar.mkEq(false, a, c2);
+    var bEquals = LiaStar.mkEq(false, b, c0);
 
     var sum = LiaStar.mkSum(
-        false, Arrays.asList(new String[] {"z","z"}), Arrays.asList(new String[] {"x", "y"}), body);
-    var f = LiaStar.mkConjunction(false, Arrays.asList(new LiaStar[] {zEquals1, sum}));
+        false, Arrays.asList(new String[] {"a","b"}), Arrays.asList(new String[] {"x", "y"}), body);
+    var f = LiaStar.mkConjunction(false, Arrays.asList(new LiaStar[] {aEquals, bEquals, sum}));
     var result = LiaSolver.solveWithConfig(f, LIA_SOLVER_CONFIGS[1]);
     System.out.print(result);
+    assertEquals(LiaSolverStatus.SAT, result);
+  }
+
+   @Test
+  public void example1With5()
+  {
+    // y >= x - 5 and y >= 5 - x    
+    var ineq1 = LiaStar.mkLe(false, LiaStar.mkPlus(false, x, cMinus5), y);
+    var minusX = LiaStar.mkMul(false, cMinus1, x);
+    var ineq2 = LiaStar.mkLe(false, LiaStar.mkPlus(false, c5, minusX), y);
+    var body = LiaStar.mkConjunction(false, Arrays.asList(new LiaStar[] {ineq1, ineq2}));
+
+    // z = 1
+    var zEquals = LiaStar.mkEq(false, a, c1);
+
+    var sum = LiaStar.mkSum(
+        false, Arrays.asList(new String[] {"a","a"}), Arrays.asList(new String[] {"x", "y"}), body);
+    var f = LiaStar.mkConjunction(false, Arrays.asList(new LiaStar[] {zEquals, sum}));
+    var result = LiaSolver.solveWithConfig(f, LIA_SOLVER_CONFIGS[1]);
+    System.out.print(result);
+    assertEquals(LiaSolverStatus.UNSAT, result);
   }
 
   @Test
